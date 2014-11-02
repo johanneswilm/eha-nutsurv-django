9ebdcf4001653d82e5e165b212fffeb8b8376279
Johannes Wilm <johannes@wilm.no>
2014-11-03 00:18:33 +0100

    add last data receipt button

--- nutsurv/dashboard/static/js/index.js
+++ nutsurv/dashboard/static/js/index.js
@@ -1,6 +1,7 @@
 var dataGetter = {
     urls: {}, // All the urls to get data from in the format  'http://.../': [triggerFunctions], where triggerFunctions is a list of functions to be called IF the data has changed since the last update.
     data: {}, // Latest data obtained from urls by url
+    updatedSources: 0,
 
     addNew: function(url,triggerFunction) { // A function to be called when registring a new trigegrFunction for a given url.
         if (url in dataGetter.urls) {
@@ -23,15 +24,32 @@ var dataGetter = {
             } else {
                 console.log('no change for '+url);
             }
+            dataGetter.updatedSources++;
+            if (dataGetter.updatedSources===Object.keys(dataGetter.urls).length) {
+                dataGetter.lastUpdate = new Date();
+            }
         })
     },
 
     getAll: function() {// Function to iniate getting data from all registered urls.
+        dataGetter.updatedSources = 0;
         _.each(_.keys(dataGetter.urls), dataGetter.getData);
     },
-
+    lastUpdate: false,
+    drawTimer : function () {
+        if (dataGetter.lastUpdate) {
+          var totalSeconds = parseInt((new Date()-dataGetter.lastUpdate)/1000),
+            minutes = Math.floor(totalSeconds / 60),
+            seconds = totalSeconds - minutes * 60;
+          if (seconds<10) {
+            seconds = '0'+seconds;
+          }
+          jQuery('#last_update span.ui-button-text').html(minutes+':'+seconds);
+       }
+    },
     setTimer: function () {// Set the timer to get data from URLs every X minutes;
         setInterval(dataGetter.getAll,5*60000); // Timer set to X*60000 milliseconds (X is minutes)
+        setInterval(dataGetter.drawTimer,1000); // Redraw timer every second
     }
 
 };
