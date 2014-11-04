7934ede6ae0de69146092e15a434dd770fca5dce
Johannes Wilm <johannes@wilm.no>
2014-11-04 11:32:22 +0100

    make data getter work with constant data

--- nutsurv/dashboard/static/js/data-getter.js
+++ nutsurv/dashboard/static/js/data-getter.js
@@ -1,16 +1,24 @@
 var dataGetter = {
-    urls: {}, // All the urls to get data from in the format  'http://.../': [triggerFunctions], where triggerFunctions is a list of functions to be called IF the data has changed since the last update.
+    urls: {},
+    /* All the urls to get data from in the format  'http://.../': {triggerFunctions:[triggerFunctions], updatable: true/false},
+    where triggerFunctions is a list of functions to be called if the data has changed since the last update and updatable specifies
+    whether the data may be updated. */
     data: {}, // Latest data obtained from urls by url
     updatedSources: 0,
+    updatableSources: 0,
 
-    addNew: function(url,triggerFunction) { // A function to be called when registring a new trigegrFunction for a given url.
+    addNew: function(url, triggerFunction, updatable) { // A function to be called when registring a new trigegrFunction for a given url.
+        // TODO: What if certain data is seen as being updateable by one tab and constant by another?
         if (url in dataGetter.urls) {
             // The url has been registered already. Add the trigegr fucntion to this datagetter and call the trigegr function with the latest data obtained from the url.
-            dataGetter.urls[url].push(triggerFunction);
+            dataGetter.urls[url].triggerFunctions.push(triggerFunction);
             triggerFunction(dataGetter.data[url]);
         } else {
-            dataGetter.urls[url] = [triggerFunction];
+            dataGetter.urls[url] = {triggerFunctions:[triggerFunction], updatable: updatable};
             dataGetter.getData(url);
+            if (updatable) {
+                dataGetter.updatableSources++;
+            }
         }
     },
 
@@ -25,11 +33,11 @@ var dataGetter = {
                 console.log('no change for '+url);
             }
             dataGetter.updatedSources++;
-            if (dataGetter.updatedSources===Object.keys(dataGetter.urls).length) {
+            if (dataGetter.updatedSources===dataGetter.updatableSources) {
                 dataGetter.lastUpdate = new Date();
                 dataGetter.setTimer();
             }
-        })
+        });
     },
 
     getAll: function() {// Function to iniate getting data from all registered urls.
