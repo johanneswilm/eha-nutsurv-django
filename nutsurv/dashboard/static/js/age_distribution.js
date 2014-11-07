9ff1c09ee3827e9711662749902deb417057360d
Johannes Wilm <johannes@wilm.no>
2014-11-07 21:27:59 +0100

    child data

--- nutsurv/dashboard/static/js/age_distribution.js
+++ nutsurv/dashboard/static/js/age_distribution.js
@@ -9,9 +9,11 @@ var ageDistribution = {
             change: ageDistribution.changeStateOrTeam
         });
         ageDistribution.drawHouseholdAgeDistribution();
+        ageDistribution.drawChildrenAgeDistribution();
         dataGetter.addNew(ageDistribution.urls.teams, ageDistribution.fillTeamsList, false);
         dataGetter.addNew(ageDistribution.urls.states, ageDistribution.fillStatesList, false);
         dataGetter.addNew(ageDistribution.urls.survey, ageDistribution.updateHouseholdAgeDistribution, true);
+        dataGetter.addNew(ageDistribution.urls.survey, ageDistribution.updateChildrenAgeDistribution, true);
     },
     fillTeamsList: function(data) {
         _.each(data.teams, function(names, id) {
@@ -27,6 +29,7 @@ var ageDistribution = {
             team = jQuery('#age_distribution_teams').val(),
             state = jQuery('#age_distribution_states').val();
         ageDistribution.updateHouseholdAgeDistribution(data,team,state);
+        ageDistribution.updateChildrenAgeDistribution(data,team,state);
     },
     fillStatesList: function(data) {
         _.each(data.states.sort(), function(state) {
@@ -47,10 +50,32 @@ var ageDistribution = {
                 },
             },
             yaxis: {
-                tickDecimals: 0
+                tickDecimals: 0,
+                min: 0
             },
             xaxis: {
-                tickDecimals: 0
+                tickDecimals: 0,
+                min: 0
+            }
+        });
+    },
+    childrenAgeDistributionPlot: false,
+    drawChildrenAgeDistribution: function() {
+        ageDistribution.childrenAgeDistributionPlot = jQuery.plot('#age_distribution_children_chart', [], {
+            series: {
+                color: "#2779AA",
+                bars: {
+                    show: true,
+                    fillColor: "#D7EBF9",
+                },
+            },
+            yaxis: {
+                tickDecimals: 0,
+                min: 0
+            },
+            xaxis: {
+                tickDecimals: 0,
+                min: 0
             }
         });
     },
@@ -78,6 +103,35 @@ var ageDistribution = {
         ageDistribution.householdAgeDistributionPlot.setData([ageList]);
         ageDistribution.householdAgeDistributionPlot.setupGrid();
         ageDistribution.householdAgeDistributionPlot.draw();
+    },
+    updateChildrenAgeDistribution: function(data, team, state) {
+        var ages = {},
+            ageList;
+        _.each(data.survey_data, function(survey) {
+            if (team && team > 0 && team != survey.team) {
+                return true;
+            }
+            if (state && state != 'All states' && state != clusterInfo.findState(survey.cluster)) {
+                return true;
+            }
+            if (survey.hasOwnProperty('child_surveys')) {
+                _.each(survey.child_surveys, function(child) {
+                    var childAge = new Date(new Date()-new Date(child.birthdate)),
+                        months = (childAge.getYear()-70)*12+childAge.getMonth();
+                    if (ages.hasOwnProperty(months)) {
+                        ages[months] ++;
+                    } else {
+                        ages[months] = 1;
+                    }
+                });
+            }
+        });
+        ageList = _.map(ages, function(num, age) {
+            return [parseInt(age), num];
+        });
+        ageDistribution.childrenAgeDistributionPlot.setData([ageList]);
+        ageDistribution.childrenAgeDistributionPlot.setupGrid();
+        ageDistribution.childrenAgeDistributionPlot.draw();
     }
 };
 
