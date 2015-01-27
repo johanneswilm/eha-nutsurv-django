047eca64aec4dff425513f3367049d9c1b4f714b
Tomasz J. Kotarba <tomasz@kotarba.net>
2015-01-27 17:59:23 +0000

    Added chi-squared and binomial test to trigger sex ration alerts for children.  Also added some utility functions related to that.

--- nutsurv/dashboard/models.py
+++ nutsurv/dashboard/models.py
@@ -1,3 +1,9 @@
+import datetime
+import dateutil.parser
+import dateutil.relativedelta
+
+import scipy.stats
+
 from django.db import models
 from django.core.validators import MinValueValidator
 
@@ -34,6 +40,166 @@ class JSONDocument(models.Model):
                 return json_document_type
         return None
 
+    @classmethod
+    def find_all_surveys_by_team(cls, team_id):
+        """This method finds all instances of JSONDocument which can be
+        associated with team given by team_id.  If team_id is None, the method
+        finds those where team_id is None (unlikely) or where the JSON document
+        does not contain an object named 'team' containing another object named
+        'team_id' (useful to find filter out documents non conforming with the
+        survey data structure specification).
+        """
+        docs = cls.objects.all()
+        output = []
+        for doc in docs:
+            tid = doc.get_team_id()
+            if tid == team_id:
+                output.append(doc)
+        return output
+
+    def find_all_surveys_by_this_team(self):
+        """This method finds all instances of JSONDocument which were created
+        by team who created this instance.  If this document is does not have
+        a valid team/team_id or the team_id is None (in both cases the document
+        is not a valid survey data structure), it returns an empty list.
+        """
+        team_id = self.get_team_id()
+        if team_id is None:
+            return []
+        else:
+            return self.find_all_surveys_by_team(team_id)
+
+    def get_team_id(self):
+        try:
+            team_id = self.json['team']['teamID']
+        except KeyError:
+            return None
+        else:
+            return team_id
+
+    def get_team_leader_name(self):
+        """Returns a team leader name or None if no team leader name found.
+        """
+        last_name = None
+        try:
+            team_members = self.json['team']['members']
+            for m in team_members:
+                designation = m['designation']
+                if designation is not None:
+                    if 'Leader' in designation:
+                        last_name = m['lastName']
+                        break
+        except KeyError:
+            return None
+        if last_name:
+            return last_name
+        else:
+            return None
+
+    def get_team_name(self):
+        """Returns a team leader name or team ID if no team leader name found or
+         'UNNAMED'.
+        """
+        team_leader = self.get_team_leader_name()
+        if team_leader is not None:
+            return team_leader
+        team_id = self.get_team_id()
+        if team_id is not None:
+            return str(team_id)
+        else:
+            return 'UNNAMED'
+
+    def _get_records_for_survey_type(self, survey_type):
+        output = []
+        if 'members' in self.json:
+            # Get the survey date to calculate age based on date of birth.
+            if 'startTime' in self.json:
+                survey_start_time = self.json['startTime']
+            else:
+                survey_start_time = None
+            for member in self.json['members']:
+                if member['surveyType'] == survey_type:
+                    member['survey_start_time'] = survey_start_time
+                    output.append(member)
+        return output
+
+    def get_child_records(self):
+        return self._get_records_for_survey_type(survey_type='child')
+
+    def get_women_records(self):
+        return self._get_records_for_survey_type(survey_type='women')
+
+    @staticmethod
+    def get_household_member_gender(household_member):
+        if 'gender' in household_member:
+            return household_member['gender']
+        else:
+            return None
+
+    @staticmethod
+    def get_childs_age_in_months(child):
+        """This function tries to calculate the child's age in months based on
+        their date of birth and the date when the survey was conducted.  If the
+        recorded data does not allow that, the function tries to use the age in
+        months recorded during the survey.
+
+        N.B. The age in years is not used by this function as a fallback even
+        if it was recorded by the surveying team.
+
+        On success, it returns an integer representing the child's age in
+        months.
+        On failure, it returns None.
+        """
+        # Start by trying to calculate the child's age based on their date of
+        # birth, if it has been recorder, as it has higher priority than the
+        # recorder age in months.
+        dob = child['survey']['birthDate']
+        # On success, the following if-statement FINISHES the function execution
+        # and returns a computed value.
+        if dob:
+            # Try to get the survey start date to use it later for the child's
+            # age computations.
+            survey_start_time_string = child['survey_start_time']
+            try:
+                survey_date = dateutil.parser.parse(survey_start_time_string)
+            except TypeError:
+                # It is impossible to compute the age at the time without
+                # knowing when the survey was conducted.
+                survey_date = None
+            else:
+                # if it worked, get rid of the TZ information and time
+                survey_date = survey_date.date()
+
+            if survey_date is not None:
+                try:
+                    dob = dateutil.parser.parse(dob)
+                except TypeError:
+                    # It is impossible to compute the age at the time without
+                    # knowing the person's date of birth.
+                    dob = None
+                else:
+                    # if it worked, get rid of the TZ info and time
+                    dob = dob.date()
+
+            if isinstance(dob, datetime.date) and \
+                    isinstance(survey_date, datetime.date):
+                delta = dateutil.relativedelta.relativedelta(survey_date, dob)
+                # return the computed age in months
+                return delta.years * 12 + delta.months
+            else:
+                dob = None  # something must have gone wrong
+
+        # The following lines should be executed only if the attempt to
+        # calculate the child's age based on their date of birth (above) was
+        # unsuccessful.
+        if dob is None:
+            try:
+                age_in_months = int(child['survey']['ageInMonths'])
+            except ValueError:
+                return None
+            else:
+                return age_in_months
+
 
 class JSONDocumentType(models.Model):
     class Meta:
@@ -66,6 +232,10 @@ class JSONDocumentType(models.Model):
 
 
 class Alert(models.Model):
+    """New alerts are only created if there is no unarchived alert having the
+    same content of the text field.  The user may still create alerts manually
+    using the admin interface.
+    """
     text = models.TextField()
     archived = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
@@ -77,6 +247,57 @@ class Alert(models.Model):
         else:
             archived = u''
         return u'{} (alert #{}{})'.format(self.text, self.pk, archived)
+
+    @classmethod
+    def run_alert_checks_on_document(cls, json_document):
+        """This method runs all the defined alert checks which leads to
+        relevant alerts being created in case they are triggered by data stored
+        in json_document.
+        """
+        cls.sex_ratio_alert(json_document)
+
+    @classmethod
+    def sex_ratio_alert(cls, json_document, test='binomial'):
+        """In the data of children from 0-59 months of age, we expect to find
+        an even number boys and girls. If chi-square of sex ratio < 0.001 then
+        report an alert on dashboard "Sex-ratio issue in team NAME".
+        N.B. This method uses binomial two-tailed test by default.  Chi-square
+        test is used only if argument test is not set to 'binomial'.
+        """
+        surveys = json_document.find_all_surveys_by_this_team()
+        children = []
+        for survey in surveys:
+            children.extend(survey.get_child_records())
+        boys = 0
+        girls = 0
+        for child in children:
+            age = JSONDocument.get_childs_age_in_months(child)
+            if age is None:
+                continue
+            elif age < 60:
+                gender = JSONDocument.get_household_member_gender(child)
+                if gender == 'M':
+                    boys += 1
+                elif gender == 'F':
+                    girls += 1
+        if test == 'binomial':
+            p = scipy.stats.binom_test([boys, girls], p=0.5)
+        else:
+            expected = (boys + girls) / 2.0
+            chi2, p = scipy.stats.chisquare([boys, girls], [expected, expected])
+        if p < 0.001:
+            team = json_document.get_team_name()
+            alert_text = 'Sex-ratio issue in team {}'.format(team)
+            # Only add if there is no same alert among unarchived.
+            if not Alert.objects.filter(text=alert_text, archived=False):
+                Alert.objects.create(text=alert_text)
+
+    @classmethod
+    def delete_alerts_by_text(cls, text, archived=False):
+        """This method tries to find alerts having the same content as given by
+        text and delete them.
+        """
+        cls.objects.filter(text=text, archived=archived).delete()
 
 
 class ClustersJSON(models.Model):
@@ -550,7 +771,6 @@ class ClustersPerState(UniqueActiveNamedDocument):
             }
         """
     )
-
 
 
 class States(UniqueActiveNamedDocument):
