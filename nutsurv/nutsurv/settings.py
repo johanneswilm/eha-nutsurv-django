6e4ead390364f79bbcae13ddac01e15eb3ec6b2e
Filip Noetzel <filip@j03.de>
2015-03-18 17:54:52 +0100

    also render browsable api (at last)

--- nutsurv/nutsurv/settings.py
+++ nutsurv/nutsurv/settings.py
@@ -138,8 +138,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.TemplateHTMLRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
 }
 
