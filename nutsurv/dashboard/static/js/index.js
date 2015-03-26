327b7a5b762469862b06c35cf71450a18f0f6beb
Brennan Novak <hi@brennannovak.com>
2015-03-26 17:24:56 +0100

    added CSRF header to all vulnerable request types, fixes #213

--- nutsurv/dashboard/static/js/index.js
+++ nutsurv/dashboard/static/js/index.js
@@ -1,4 +1,20 @@
 jQuery(document).ready(function() {
+
     // Dropdowns
     $('.dropdown-toggle').dropdown();
+
+    // For CSRF Protection
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));
+            }
+        }
+    });
+
 });
