c456245e6737ef21c124a8d5e5b182ddb24dfb03
Johannes Wilm <mail@johanneswilm.org>
2015-04-13 10:17:30 +0200

    add team_name as well

--- nutsurv/dashboard/models.py
+++ nutsurv/dashboard/models.py
@@ -488,6 +488,7 @@ class Alert(models.Model):
         location = household_survey.get_location()
         team_lead = household_survey.team_lead
         team_id = household_survey.get_team_id()
+        team_name = household_survey.get_team_name()
 
         if cluster_id is None:
             alert_text = 'No cluster ID for survey of team {} (survey {})'.format(
