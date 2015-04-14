d3f970cc0bacacb474956db6a0dacf91db0ee8f6
Brennan Novak <hi@brennannovak.com>
2015-04-14 17:18:20 +0200

    made some progress with Personnel design as per issue #276

--- nutsurv/dashboard/static/js/personnel.js
+++ nutsurv/dashboard/static/js/personnel.js
@@ -41,35 +41,42 @@ var personnel = {
             personnelData = dataGetter.downloads[personnel.urls.personnel].data.personnel,
             clusterData = dataGetter.downloads[personnel.urls.clusterData].data.clusters, // Cluster data not actually used directly in this function, but we need t make sure it is there for clusterInfo
             perPersonnelData = [];
+
+        var genderIcon = {
+          M: '<i class="fa fa-male"></i> ',
+          F: '<i class="fa fa-female"></i> '
+        }
+
         _.each(personnelData, function(person, id) {
+
             var personnelObject = {
-                    personnel_id: id,
-                    name: person.name,
-                    gender: person.gender,
-                    phone: person.phone,
-                    email: person.email,
-                    position: person.position,
-                    first_admin_level: '',
-                    second_admin_level: '',
-                    cluster: '',
-                    cluster_number: '',
-                    date: '',
-                    team: person.team,
-                    age: person.age.toString()
-                };
+                gender: genderIcon[person.gender],
+                name: '<strong>' + person.name + '</strong><br> 33 years',
+                personnel_id: id,
+                contact: '<a href="mailto:' + person.email + '">' + person.email + '</a><br>' + person.phone,
+                position: person.position,
+                first_admin_level: '',
+                second_admin_level: '',
+                cluster: '',
+                date: '',
+                team: person.team,
+                age: ''
+            };
 
             perPersonnelData.push(personnelObject);
         });
 
         _.each(surveyData, function(survey, id) {
+
+            // Join on "team"
             var teamMembers = _.where(perPersonnelData, {team: survey.team}),
             surveyDate = survey.endTime.split('T')[0];
 
             if (teamMembers.length > 0 && teamMembers[0].date < surveyDate) {
                 _.each(teamMembers, function(teamMember) {
-                    teamMember.date = surveyDate;
-                    teamMember.cluster_number = survey.cluster;
-                    teamMember.cluster = clusterInfo.findName(survey.cluster);
+                    teamMember.position = teamMember.position + '<br>' + moment(surveyDate).format('MMM D YY');
+                    teamMember.date = 'Team Leader<br>' + moment(surveyDate).format('MMM D YY');
+                    teamMember.cluster = clusterInfo.findName(survey.cluster) + ' #' + survey.cluster;
                     teamMember.first_admin_level = clusterInfo.findFirstAdminLevel(survey.cluster);
                     teamMember.second_admin_level = clusterInfo.findSecondAdminLevel(survey.cluster);
                 });
@@ -77,57 +84,54 @@ var personnel = {
         });
 
         if (personnel.table) {
-            // If the table exists already, we destroy it, as it cannot easily be reinitialized.
+            // If the table exists already, we destroy it
+            // as it cannot easily be reinitialized.
             personnel.table.fnDestroy();
         }
 
         personnel.table = jQuery('#personnel_table').dataTable({
-            dom: '<"#personnel_download">lfrtip',
+            paging: true,
+            ordering: true,
+            searching: true,
+            dom: '<"#personnel_data" <"page-header"fr<"clear">><t><"bottom">ip<"clear">>',
             data: perPersonnelData,
             responsive: {
-                        details: {
-                            renderer: function ( api, rowIdx ) {
-                                // Select hidden columns for the given row
-                                var data = api.cells( rowIdx, ':hidden' ).eq(0).map( function ( cell ) {
-                                    var header = jQuery( api.column( cell.column ).header() );
-                                    return '<tr>'+
-                                            '<td>'+
-                                                header.attr('data-column-name')+':'+
-                                            '</td> '+
-                                            '<td>'+
-                                                api.cell( cell ).data()+
-                                            '</td>'+
-                                        '</tr>';
-                                } ).toArray().join('');
+                details: {
+                    renderer: function(api, rowIdx) {
+                        // Select hidden columns for the given row
+                        console.log(api);
+                        var data = api.cells(rowIdx, ':hidden').eq(0).map( function(cell) {
+                            var header = jQuery( api.column( cell.column ).header() );
+                            return '<tr>'+
+                                    '<td>'+ header.attr('data-column-name')+':'+'</td> '+
+                                    '<td>'+ api.cell(cell).data()+'</td>'+
+                                '</tr>';
+                        }).toArray().join('');
 
-                                return data ?
-                                    $('<table/>').append( data ) :
-                                    false;
-                            },
-                        }
+                        return data ? $('<table/>').append( data ) : false;
                     },
+                }
+            },
             columns: [
-                { "searchable": false, data: function(){return '';}, orderable: false },
-                { name: 'personnel_id', data: 'personnel_id' },
-                { name: 'name', data: 'name' },
-                { name: 'position', data: 'position' },
-                { name: 'age', data: 'age' },
                 { name: 'gender', data: 'gender' },
-                { name: 'phone', data: 'phone' },
-                { name: 'email', data: 'email' },
-                { name: 'date', data: 'date' },
+                { name: 'name', data: 'name' },
+                { name: 'personnel_id', data: 'personnel_id' },
+                { name: 'contact', data: 'contact' },
+                { name: 'position', data: 'position' },
+                { name: 'cluster', data: 'cluster' },
                 { name: 'first_admin_level', data: 'first_admin_level' },
                 { name: 'second_admin_level', data: 'second_admin_level' },
-                { name: 'cluster', data: 'cluster' },
-                { name: 'cluster_number', data: 'cluster_number' }
+                { "searchable": false, data: function() { return ''; }, orderable: false }
             ],
             "order": [[ 1, "asc" ]]
         });
 
-        jQuery('#personnel_download').html('<button></button>');
-        jQuery('#personnel_download button').addClass('btn btn-default dataTables_extra_button');
-        jQuery('#personnel_download button').text('Download');
+        // Add UI Items
+        $('.page-header').append('<h1><i class="fa fa-user"></i> Personnel</h1>');
+        $('#personnel_table_filter').addClass('pull-right');
+        $('.page-header').prepend('<button class="pull-right btn btn-default dataTables_extra_button">Download</button>');
 
+        // Download Button Action
         jQuery('#personnel_download button').on('click', function (){
             personnel.downloadData();
         });
