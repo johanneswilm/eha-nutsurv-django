26303a993a90bc9095a882302bdf178fcf6cf31e
Tim Watts <tim.watts@ehealthnigeria.org>
2015-04-15 10:28:18 +0000

    add migration

---
+++ nutsurv/dashboard/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dashboard', '0007_auto_20150413_0908'),
+        ('dashboard', '0006_merge'),
+    ]
+
+    operations = [
+    ]
