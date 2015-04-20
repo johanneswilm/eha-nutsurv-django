8f77fbea20003d26e7854612ef2f483873a02beb
Tim Watts <tim.watts@ehealthnigeria.org>
2015-04-20 16:07:34 +0000

    Fix migrations

--- nutsurv/dashboard/models.py
+++ nutsurv/dashboard/models.py
@@ -184,10 +184,10 @@ class BaseHouseholdSurveyJSON(models.Model):
 
     second_admin_level = models.CharField(max_length=20, blank=True)
     first_admin_level = models.CharField(max_length=20, blank=True)
-    cluster = models.IntegerField()
+    cluster = models.IntegerField(blank=True, null=True)
     cluster_name = models.CharField(max_length=30, blank=True)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
     uuid = models.CharField(
         max_length=255, unique=True,
         help_text='A unique identifier of an individual household survey.  '
