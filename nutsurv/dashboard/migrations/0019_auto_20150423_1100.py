177d67d7a8eb1b64284f4b6268ca2cd516d4003b
Filip Noetzel <filip@j03.de>
2015-04-23 15:04:12 +0200

    remove stupid migration

--- nutsurv/dashboard/migrations/0019_auto_20150423_1100.py
+++ nutsurv/dashboard/migrations/0019_auto_20150423_1100.py
@@ -24,10 +24,4 @@ class Migration(migrations.Migration):
             field=models.CharField(default=b'general', max_length=255, choices=[(b'general', b'general'), (b'map', b'map'), (b'sex', b'sex'), (b'age_distribution', b'age_distribution'), (b'number_distribution', b'number_distribution'), (b'timing', b'timing')]),
             preserve_default=True,
         ),
-        migrations.AlterField(
-            model_name='householdsurveyjson',
-            name='json',
-            field=jsonfield.fields.JSONField(help_text=b'A JSON document containing data acquired from one household.  Typically not edited here but uploaded from a mobile application used by a team of surveyors in the field.  If in doubt, do not edit.'),
-            preserve_default=True,
-        ),
     ]
