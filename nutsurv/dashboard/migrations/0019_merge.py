b6de283bf0e91c9c72a8e1695c0cfb5cc8a6a928
Brennan Novak <hi@brennannovak.com>
2015-04-24 13:19:10 +0200

    added migration files

---
+++ nutsurv/dashboard/migrations/0019_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dashboard', '0011_auto_20150422_1249'),
+        ('dashboard', '0018_auto_20150421_1508'),
+    ]
+
+    operations = [
+    ]
