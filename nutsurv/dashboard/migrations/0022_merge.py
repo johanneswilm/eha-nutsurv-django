9ac763128f09cdd225e2218a891b57d441d51576
Filip Noetzel <filip@j03.de>
2015-04-28 15:01:01 +0200

    add merge migration

---
+++ nutsurv/dashboard/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dashboard', '0019_auto_20150423_1100'),
+        ('dashboard', '0021_merge'),
+    ]
+
+    operations = [
+    ]
