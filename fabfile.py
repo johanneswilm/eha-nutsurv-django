fad38adb435459a75b62dc77b65b1236da27ea9c
Filip Noetzel <filip@j03.de>
2015-05-07 15:57:35 +0200

    amend fabfile to also support deploying to staging

--- fabfile.py
+++ fabfile.py
@@ -29,7 +29,7 @@ def build_dockerimage(branch_or_tag='develop'):
       run('docker push docker-registry.eocng.org/ehealthafrica/nutsurv:{}'.format(
         branch_or_tag))
 
-@roles('dev')
+@roles('dev', 'staging')
 def deploy(branch_or_tag=None):
 
     if env.host_string == 'ubuntu@nutsurv-dev.eocng.org':
@@ -37,12 +37,18 @@ def deploy(branch_or_tag=None):
           branch_or_tag = 'develop'
 
     else:
-        assert branch_or_tag.startswith('v'), "You can only deploy tags in this environment"
+        assert branch_or_tag, "Please specify a tag"
+        assert branch_or_tag.startswith('v'), "You can only deploy tags to {}".format(env.host_string)
 
     run('docker pull docker-registry.eocng.org/ehealthafrica/nutsurv:{}'.format(
         branch_or_tag))
 
-    run('docker stop nutsurvdeploy_web_1')
+    stop_result = run('docker stop nutsurvdeploy_web_1', warn_only=True)
+
+    if (stop_result and
+        stop_result != 'nutsurvdeploy_web_1' and
+        not 'Error response from daemon: No such container:' in stop_result):
+      assert False, stop_result
 
     upload_template(
         'docker-compose-deploy.yml.template',
