0d1fd247ce1fe7785e7fbf29df7d59dbd097bcab
Filip Noetzel <filip@j03.de>
2015-05-12 12:01:59 +0200

    split fab 'deploy' to 'deploy' and 'up'

--- fabfile.py
+++ fabfile.py
@@ -31,7 +31,6 @@ def build_dockerimage(branch_or_tag='develop'):
         run('docker push docker-registry.eocng.org/ehealthafrica/nutsurv:{}'.format(
             branch_or_tag))
 
-
 @roles('dev', 'staging', 'production')
 def deploy(branch_or_tag=None):
 
@@ -59,6 +58,9 @@ def deploy(branch_or_tag=None):
         context={'branch_or_tag': branch_or_tag, }
     )
 
+
+@roles('dev', 'staging', 'production')
+def up():
     with cd('~/nutsurv_deploy'):
         run('docker-compose -f ~/nutsurv_deploy/docker-compose-deploy.yml up -d web')
 
