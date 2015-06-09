04b112902aaedbdd239d8fa6442bb01da2456b23
adamvert <ab@adam-butler.com>
2015-06-09 14:38:00 +0200

    Unify team & stratum dropdown implementations

--- nutsurv/dashboard/static/js/team_strata_selector.js
+++ nutsurv/dashboard/static/js/team_strata_selector.js
@@ -1,48 +1,57 @@
-(function() {
+teamStrataSelectors = {
 
-function read_querystring() {
-  var qs = {};
-  window.location.search.substring(1).split('&').forEach(function(el) {
-    el = el.split('=');
-    qs[el[0]] = el[1];
-  });
-  return qs;
-}
+  read_querystring: function() {
+    var qs = {};
+    window.location.search.substring(1).split('&').forEach(function(el) {
+      el = el.split('=');
+      qs[el[0]] = el[1];
+    });
+    return qs;
+  },
 
-function change_querystring() {
-  window.location.search = '?' + jQuery.param({
-    team_lead: $("#team_lead_selector").val(),
-    stratum: $("#strata_selector").val(),
-  });
-}
+  change_querystring: function() {
+    window.location.search = '?' + jQuery.param({
+      team_lead: $("#team_lead_selector").val(),
+      stratum: $("#strata_selector").val(),
+    });
+  },
 
-function fill_selectpicker(sel, template, data, selected_value) {
-  var el = jQuery(sel);
-  var tmpl = _.template(template);
-  _.each(data, function(names, id) {
-      el.append(tmpl({id: id, names: names}));
-  });
-  el.selectpicker();
-  el.selectpicker('val', selected_value);
-  el.on('change', change_querystring);
-}
+  fill_selectpicker : function(sel, template, data, selected_value, callback) {
+    var el = jQuery(sel);
+    var tmpl = _.template(template);
+    _.each(data, function(datum, id) {
+        if (_.isObject(datum)) {
+          el.append(tmpl({id: id, names: datum}));
+        } else {
+          el.append(tmpl({first_admin_level_name: datum}));
+        }
+    });
+    el.selectpicker();
+    el.selectpicker('val', selected_value);
 
-$.get('/dashboard/teammembers/', function (data) {
-  fill_selectpicker(
-    "#team_lead_selector",
-    '<option value="<%- names.id %>"><%- names.firstName %> <%- names.lastName %></option>',
-    data,
-    read_querystring()['team_lead']
-  );
-});
+    el.on('change', callback || this.change_querystring);
+  },
 
-$.get('/dashboard/clustersjsonview/', function (data) {
-  fill_selectpicker(
-    "#strata_selector",
-    '<option value="<%- names.first_admin_level_name %>" ><%- names.cluster_name %></option>',
-    data.clusters,
-    read_querystring()['stratum']
-  );
-});
+  init: function(callback) {
+    var scope = this;
+    $.get('/dashboard/teammembers/', function (data) {
+      scope.fill_selectpicker(
+        "#team_lead_selector",
+        '<option value="<%- names.id %>"><%- names.firstName %> <%- names.lastName %></option>',
+        data,
+        scope.read_querystring()['team_lead'],
+        callback
+      );
+    });
 
-}());
+    $.get('/dashboard/firstadminleveljsonview/', function (data) {
+      scope.fill_selectpicker(
+        "#strata_selector",
+        '<option value="<%- first_admin_level_name %>" ><%- first_admin_level_name %></option>',
+        data.first_admin_levels,
+        scope.read_querystring()['stratum'],
+        callback
+      );
+    });
+  }
+};
