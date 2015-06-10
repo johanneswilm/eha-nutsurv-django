24f6c6391e72d60effe73991b26371da246c066b
adamvert <ab@adam-butler.com>
2015-06-10 10:52:52 +0200

    ... and add the mapConfig file

---
+++ nutsurv/dashboard/static/js/map-config.js
@@ -0,0 +1,37 @@
+var mapConfig = {
+    osmUrl: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    markers: {
+        green: L.icon({
+            iconUrl: '/static/images/marker-green.png',
+            iconSize: [14, 14],
+            shadowSize: [20, 14],
+            iconAnchor: [10, 7],
+            shadowAnchor: [20, 7],
+            popupAnchor: [-3, -5]
+        }),
+        red: L.icon({
+            iconUrl: '/static/images/marker-red.png',
+            iconSize: [14, 14],
+            shadowSize: [20, 14],
+            iconAnchor: [10, 7],
+            shadowAnchor: [20, 7],
+            popupAnchor: [-3, -5]
+        }),
+        gray: L.icon({
+            iconUrl: '/static/images/marker-gray.png',
+            iconSize: [14, 14],
+            shadowSize: [20, 14],
+            iconAnchor: [10, 7],
+            shadowAnchor: [20, 7],
+            popupAnchor: [-3, -5]
+        }),
+        yellow: L.icon({
+            iconUrl: '/static/images/marker-yellow.png',
+            iconSize: [14, 14],
+            shadowSize: [20, 14],
+            iconAnchor: [10, 14],
+            shadowAnchor: [20, 7],
+            popupAnchor: [-3, -5]
+        }),
+    },
+};
