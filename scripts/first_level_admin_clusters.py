ebdfa885c7cc03587ef71a0fe8a387f779a99dc8
Tim Watts <tim.watts@ehealthnigeria.org>
2015-07-20 14:23:38 +0000

    Added cluster scripts and added source and output

---
+++ scripts/first_level_admin_clusters.py
@@ -0,0 +1,13 @@
+import csv
+import json
+
+with open('2015_06_29_NNHS_2015_Selected EA_Final.xlsx - EA_2015.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    clusterfile = {}
+    for row in reader:
+        clusterfile[row[0]] = {
+            "reserve": 5,
+            "standard": 10
+        }
+
+    print json.dumps(clusterfile, indent=2, separators=(',', ': '))
