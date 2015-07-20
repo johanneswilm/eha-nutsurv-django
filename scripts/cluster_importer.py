f4ec778e895a710576920ebef5dc246b1b4dc47d
Tom Vincent <git@tlvince.com>
2015-07-20 12:26:32 +0100

    Read over stdin

--- scripts/cluster_importer.py
+++ scripts/cluster_importer.py
@@ -16,15 +16,15 @@
 
 import csv
 import json
+import fileinput
 
-with open('2015_06_29_NNHS_2015_Selected EA_Final.xlsx - EA_2015.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    clusterfile = {}
-    for row in reader:
-        clusterfile[row[7]] = {
-          "cluster_name": row[4],
-          "second_admin_level_name": row[2],
-          "first_admin_level_name": row[0],
-        }
+reader = csv.reader(fileinput.input(), delimiter=',')
+clusterfile = {}
+for row in reader:
+    clusterfile[row[7]] = {
+      "cluster_name": row[4],
+      "second_admin_level_name": row[2],
+      "first_admin_level_name": row[0],
+    }
 
-    print json.dumps(clusterfile, indent=2, separators=(',', ': '))
+print json.dumps(clusterfile, indent=2, separators=(',', ': '))
