33ee2cdad20aab11ffdc76c4bd2bd5a82295e798
Tom Vincent <git@tlvince.com>
2015-07-22 13:24:19 +0100

    Add EA count bug note

--- scripts/first_level_admin_clusters.py
+++ scripts/first_level_admin_clusters.py
@@ -7,6 +7,9 @@ with open('2015_06_29_NNHS_2015_Selected EA_Final.xlsx - EA_2015.csv') as csvfil
     next(reader)
     for row in reader:
         clusterfile[row[0].upper()] = {
+            # FIXME this should be a real count of enumeration areas
+            # (EA/EA_NAME) per state (see #592) e.g.:
+            # { ABIA: { standard: 32, reserve: 4 } }
             "reserve": 5,
             "standard": 10
         }
